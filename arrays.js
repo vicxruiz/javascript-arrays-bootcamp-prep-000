2e92d8f886b3f3be41761dd4939b0bdceb6e97f4
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-27 04:31:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function removeElementFromBeginningOfArray(chocolateBars) {
   return chocolateBars.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
-  return chocolateBars.slice(-1)
+  return chocolateBars.pop()
 }
 
 
