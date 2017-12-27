2528cf902b83a1b6a086f852f76d76000d48553a
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-27 04:35:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,8 +29,7 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
   return chocolateBars
 }
 function removeElementFromEndOfArray(chocolateBars) {
-  chocolateBars.slice(0, chocolateBars.length - 1)
-  return chocolateBars
+  return 
 }
 
 
