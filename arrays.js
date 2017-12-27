5db6cb11cb1021afa73558f0a48c91dece62cb5c
Victor Ruiz <ruizdvictor@gmail.com>
2017-12-27 04:14:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
 function addElementToEndOfArray(chocolateBars, element) {
   return [...chocolateBars, element]
 }
+function destructivelyAddElementToEndOfArray(chocolateBars, element) {
+  chocolateBars.push(element)
+  return chocolateBars
+}
